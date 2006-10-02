6d91e22bc8aadc28d51f69cf98fa12300adc128c
Jesse Vincent <jesse@bestpractical.com>
2006-10-02 18:01:30 +0000

    * 0.02

--- lib/CommitBit.pm
+++ lib/CommitBit.pm
@@ -3,18 +3,21 @@ package CommitBit;
 use warnings;
 use strict;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 NAME
 
-CommitBit - An opensource project administration tool
+CommitBit - A project administration tool
 
 =head1 DESCRIPTION
 
-CommitBit is a tool for managing commit access to opensource projects. It also provides for a "code.yourorganization.org" site with links to your wiki, mailing lists, bug tracking and so on.
+CommitBit is a tool for managing commit access to software projects.
+It also provides for a "code.yourorganization.org" site with links
+to your wiki, mailing lists, bug tracking and so on.
 
-The project is in the early stages of development and could desperately use some
-help. If you're interested, please write to jesse@bestpractical.com and ask for a commit bit.
+The project is in the early stages of development and could desperately
+use some help. If you're interested, please write to
+jesse@bestpractical.com and ask for a commit bit.
 
 
 =head1 AUTHORS 
@@ -29,7 +32,8 @@ Copyright 2006, Best Practical Solutions
 
 This software is freely available under the same terms as Perl 5.8.8.
 (That means you can use it under the terms of the GPL or The Artistic 
-License 1.0). If you want to use it under the terms of The Death and Repudiation License, that's cool too. (COPYING/dr.txt)
+License 1.0). If you want to use it under the terms of The Death and 
+Repudiation License, that's cool too. (COPYING/dr.txt)
 
 =cut
 
